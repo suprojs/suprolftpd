b118cdfbf3fc55c2d6eb7fd17e262ce0077be5a8
Oleg Verych <olecom@gmail.com>
2014-10-16 17:04:15 +0300

    Setup API for `lftpd`
    * add dynamic api handlers
    * move config setup logic in api call to be dynamically reconfigured
    
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- app_back_suprolftpd.js
+++ app_back_suprolftpd.js
@@ -1,46 +1,22 @@
 /*
+ * Setup API for `lftpd`
  **/
 
 module.exports = suprolftpd
 
 function suprolftpd(api, cfg){
 var app = api.app, name = 'suprolftpd'
-   ,path = require('path')
-
-    if('boolean' == typeof cfg){// simple module enabler by 'true'
-        cfg = (config_default()).suprolftpd// use default config
-    }
-
-    cfg.data_path = path.normalize(
-        //  $PWD/app_modules/suprolftpd/ -> $PWD/data/suprolftpd/
-        __dirname + '/../..' + (cfg.data_path || '/data/suprolftpd/')
-    )
-    if(cfg.bin){//              bin: '../../bin/lftp.exe'// cygwin
-        cfg.bin = path.normalize(__dirname + '/' + cfg.bin)
-    }
-    //require('./lib/mongodb.js')[cfg.bin ? 'launch' : 'connect'](
-    //    api, cfg
-    //)
-
-    /* == admin/status UI && API: == */
-    if(!cfg.rbac){
-        cfg.rbac = { can: { }}
-    } else if(!cfg.rbac.can){
-        cfg.rbac.can = { }
-    }
-    // add `can` for toolbar with daemon handlers
-    //cfg.rbac.can['App.supromongod.view.ControlTools'] = true
-    //cfg.rbac.can['/supromongod/lib/'] = true
+   ,lftpd = require('./lib/api_load.js')(api, cfg)// dynamic config
 
     return {
         css:['/css/' + name + '/css'],
         js: ['/' + name + '/app_front_' + name ],
         app_use: app_use,// call this *after* `mwBasicAuthorization()`
-        cfg: cfg
+        cfg:{ rbac: lftpd.cfg.rbac, extjs: null }// do not share whole config
     }
 
     function app_use(){
-        //app.use('/' + name + '/lib/', require('./lib/api_load.js')(api, cfg))
+        app.use('/' + name + '/lib/', lftpd.mwAPI)
         // order of priority; serve static files, css, l10n
         app.use('/' + name + '/', api.connect['static'](__dirname + '/'))
         app.use('/l10n/', api.mwL10n(api, __dirname, '_' + name + '.js'))
@@ -48,13 +24,5 @@ var app = api.app, name = 'suprolftpd'
         app.use('/css/' + name + '/css', api.connect.sendFile(
             __dirname + '/' + name + '.css', true)
         )
-    }
-
-    function config_default(){
-        return {
-        suprolftpd:{
-            bin: '../../bin/lftp.exe',// cygwin
-            data_path: ''
-        }}
     }
 }
