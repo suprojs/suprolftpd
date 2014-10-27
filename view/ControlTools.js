469455b4bf29f344b5c5dd23796893740f3f0a6d
Oleg Verych <olecom@gmail.com>
2014-10-27 05:09:32 +0300

    view/ControlTools: move tools in its class file (the end of developing)
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- view/ControlTools.js
+++ view/ControlTools.js
@@ -1,37 +1,76 @@
+/*
+ * for RAD/hot swap/reloading move this back into main view
+ */
 Ext.define('App.suprolftpd.view.ControlTools',{
     extend: Ext.toolbar.Toolbar,
     dock: 'bottom',
+    itemId: 'tools',
+    bindGrid: null,
     initComponent: function initSuprolftpdView(){
-    var me = this
+    var me = this, SM, go, quit
 
-        /*this.items = ['-','suprolftpd: ',{
-            text: l10n.stsEcho
-           ,iconCls: 'sg-e'
-           ,handler: todo
-        },'->','-',{
-            text: l10n.stsStopSystem
-           ,iconCls: 'sg-s'
-           ,handler: todo
-        },'-',{
-            text: l10n.stsRestart
-           ,iconCls: 'sg-r'
-           ,handler: todo
-        },'-','->',{
-            text: l10n.stsKill
-           ,iconCls: 'sg-k'
-           ,handler: todo
-        }]*/
+        me.items = [{
+            text: l10n.lftpd.go
+           ,iconCls: 'ld-icon-go'
+           ,disabled: true
+           ,handler: doStartStop
+        },'-','->','-',{
+            text: l10n.lftpd.quit
+           ,iconCls: 'ld-icon-quit'
+           ,disabled: true
+           ,handler: doStartStop
+        }]
+        me.bindGrid = bindGrid
         me.callParent()
 
+        go = me.down('button[iconCls=ld-icon-go]')
+        quit = me.down('button[iconCls=ld-icon-quit]')
+        // todo add/edit channel
         return
 
-        /*function todo(){
-            Ext.Msg.show({
-                title: l10n.errun_title,
-                buttons: Ext.Msg.OK,
-                icon: Ext.Msg.ERROR,
-                msg: 'TODO'
+        function bindGrid(grid){
+            SM = grid.getSelectionModel()
+            grid.on('select', select)
+            grid.store.on('datachanged', datachanged)
+        }
+
+        function datachanged(){
+        var chan = SM.getSelection()[0]
+        /// apply possibly new tool's state; NOTE: 'quit' is doubled it's OK
+            chan && select(null, chan)
+        }
+
+        function select(sm, model){
+            switch(model.data.sts[0]){
+            case 's':
+            case 'q': return quit.disable(), go.enable()// quit, stop -> 'go'
+            case 'r': return quit.enable(), go.disable()// runs -> enable `quit`
+            //todo: config existing
+            default : return quit.disable(), go.disable()
+            }
+        }
+
+        function doStartStop(btn){
+        var chan = SM.getSelection()[0]
+
+            return chan && App.backend.req(
+                '/suprolftpd/lib/cnl/do',
+                {
+                    id: chan.data.id,
+                    cmd: 'ld-icon-quit' == btn.iconCls ? 'quit' : 'start'
+                },
+            function(err, json){
+                if(!err && json && json.success){
+                    chan.set(json)
+                    return
+                }
+                Ext.Msg.show({
+                    title: l10n.errun_title,
+                    buttons: Ext.Msg.OK,
+                    icon: Ext.Msg.ERROR,
+                    msg: l10n.errun_title
+                })
             })
-        }*/
+        }
     }
 })
