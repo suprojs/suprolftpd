aa006719524c37429fded801a1972c4fa7423d4e
Oleg Verych <olecom@gmail.com>
2014-11-08 03:25:34 +0400

    fixes: status and comments
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- etc/lftpd.sh
+++ etc/lftpd.sh
@@ -10,7 +10,7 @@
 # |$ SUPRO_OBJ='LOOPBACK' PATH=../../bin ../../bin/sh -c "$PWD/etc/lftpd.sh"
 # `-----------
 
-trap 'echo "e:
+trap 'echo "
 Unexpected Script Error! Use /bin/sh -x $0 to trace it.
 "
 set +e
@@ -19,13 +19,9 @@ exit 76
 ' 0
 #exec 7>&1 1>>"lftpd.sh.log" 2>&1 && set -x && echo "$*" #debug
 set -e
-#
-# STDOUT and STDERR messages must obey one-char status e.g.:
-# `echo "r: runs ok"`
-# `echo "e: errors "`
-#
+
 _err(){
-    printf 'e:%b' "$*" >&2
+    printf '%b' "$*" >&2
 }
 
 _exit(){
@@ -33,7 +29,7 @@ _exit(){
     exit "$1"
 }
 
-[ "$SUPRO_OBJ" ] || { echo 's:
+[ "$SUPRO_OBJ" ] || { echo '
 Scripting of `lftp` under "windows-cygwin" or "linux-gnu" OSes
 env: "$SUPRO_OBJ" is object this instance is runs for; it must be set
 
@@ -50,7 +46,10 @@ env: "$SUPRO_OBJ" is object this instance is runs for; it must be set
 exit 77
 }
 
-#debug 777 -o debug.txt
+#
+# NOTE on `lftp`: don't use `queue`, `mirror` commands,
+# they are complicated and have bugs (in some versions)
+#
 LFTP_OPT='
 set cmd:interactive false
 set net:timeout 4
@@ -59,33 +58,14 @@ set net:max-retries 4
 set net:reconnect-interval-base 4
 set net:reconnect-interval-multiplier 1
 set xfer:disk-full-fatal true
-set xfer:clobber on'
+set xfer:clobber on
+'
+#debug 777 -o debug.txt
 
 _date(){ # ISO date
     date -u '+%Y-%m-%dT%H:%M:%SZ'
 }
 
-#exec lftp -e "$LFTP_OPT"
 
-while read MASTERS_CMD
-do case "$MASTERS_CMD" in
-    'echo_sh')
-        echo 'r: echo_sh'
-    ;;
-    'LFTP_OPT')
-        echo "r: $LFTP_OPT"
-    ;;
-    'quit')
-        _exit 0
-    ;;
-    *)
-        echo 'r'"`$MASTERS_CMD`" || echo "e: error running:
-====
-$MASTERS_CMD
-===
-"
-    ;;
-    esac
-done
 # this line must be reached only in console (e.g. development)
 tty && _exit 0 || _exit 2
