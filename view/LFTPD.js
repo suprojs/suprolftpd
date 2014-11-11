suprojs/suprolftpd

commit 7201ddc5e3cbecc28983878bc7d573008fe23300

view/LFTPD: bigger and more nice images for up/down
Signed-off-by: Oleg Verych <olecom@gmail.com>

view/LFTPD.js
@@ -140,13 +140,15 @@ App.cfg['App.suprolftpd.view.LFTPD'] = {// fast init
 
         function render0(value, meta){
             meta.tdAttr = 'data-qtip="' + l10n.lftpd.sts[value[0]] + '"'
-            return '<img sts src="' + App.backendURL +
+            return '<img sts style="transform:rotate(-90deg);-webkit-transform:rotate(-90deg);" src="' +
+                    App.backendURL +
                    '/css/suprolftpd/' + value[0] + '.png">'
         }
 
         function render1(value, meta){
             meta.tdAttr = 'data-qtip="' + l10n.lftpd.sts[value[1]] + '"'
-            return '<img sts src="' + App.backendURL +
+            return '<img sts style="transform:rotate(90deg);-webkit-transform:rotate(90deg);"  src="' +
+                    App.backendURL +
                    '/css/suprolftpd/' + value[1] + '.png">'
         }
 

commit 60cbdaca7192053fdcdc56986db595fea5a04d03

view/LFTPD: fix bigger and more nice images for up/down
error (or any other status image) was rotated which is bad

Fixes: 7201ddc5e3cbecc28983878bc7d573008fe23300
Signed-off-by: Oleg Verych <olecom@gmail.com>

view/LFTPD.js
@@ -140,16 +140,18 @@ App.cfg['App.suprolftpd.view.LFTPD'] = {// fast init
 
         function render0(value, meta){
             meta.tdAttr = 'data-qtip="' + l10n.lftpd.sts[value[0]] + '"'
-            return '<img sts style="transform:rotate(-90deg);-webkit-transform:rotate(-90deg);" src="' +
+            return '<img sts src="' +
                     App.backendURL +
-                   '/css/suprolftpd/' + value[0] + '.png">'
+                   '/css/suprolftpd/' + value[0] + ('r' == value[0] ? 'u' : '') +
+                   '.png">'
         }
 
         function render1(value, meta){
             meta.tdAttr = 'data-qtip="' + l10n.lftpd.sts[value[1]] + '"'
-            return '<img sts style="transform:rotate(90deg);-webkit-transform:rotate(90deg);"  src="' +
+            return '<img sts src="' +
                     App.backendURL +
-                   '/css/suprolftpd/' + value[1] + '.png">'
+                   '/css/suprolftpd/' + value[1] + ('r' == value[1] ? 'd' : '') +
+                   '.png">'
         }
 
         function render2(value, meta, model){
@@ -176,7 +178,8 @@ App.cfg['App.suprolftpd.view.LFTPD'] = {// fast init
                 } else if((el = node.dom.querySelectorAll('img[sts]')) && el.length)
                 for(i = 0; i < 4; ++i) if(mdata.prests[i] != n[i]){
                     el[i].setAttribute('src',
-                        App.backendURL + '/css/suprolftpd/' + n[i] + '.png'
+                        App.backendURL + '/css/suprolftpd/' + n[i] +
+                        ('r' == n[i] ? 0 == i ? 'u' : 'd' : '') + '.png'
                     )// asm:EXTJS4 (architecture specific model of coding)
                     el[i].parentNode.parentNode.setAttribute('data-qtip',
                         l10n.lftpd.sts[n[i]]