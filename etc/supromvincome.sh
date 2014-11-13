777259333b721efd26896efd48852f1e7fdacfd9
Oleg Verych <olecom@gmail.com>
2014-11-14 01:08:26 +0400

    lftpd.sh: remove files
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- etc/supromvincome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#unzip && rm _data/exchange/remote/13d5b3485e5_0.json.bz2.S1.GLOB >
-#`->_data/exchange/local/income/13d5b3485e5_0.json
-#debug: 
-set -x ; exec >>logunzip 2>&1
-for f in *$ZEXT*$DATAEXT
-do s=$f
-$UNZIP "$s" | dd 'status=noxfer' of="$LIDIR/${f%$ZEXT*}" && rm "$s"
-done
